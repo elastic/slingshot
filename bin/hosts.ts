b4cd53a1a3690a5ca836bf5a8e2706b8aa6ebd2d
Chris Cowan <chris@chriscowan.us>
2021-03-30 16:31:52 -0600

    Removing host script

--- bin/hosts.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ts-node
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-import { getConfig } from '../src/lib/get_config';
-import { load } from '../src/lib/load';
-const options = getConfig({}, `${__dirname}/../configs/hosts.json`);
-load(options);
